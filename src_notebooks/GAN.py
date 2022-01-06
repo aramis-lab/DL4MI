25870188fca51a77209c18f3ce71862bbdb79f9f
ravih18 <rhassana96@gmail.com>
2022-01-06 15:56:33 +0100

    discriminator block now return a Sequential instead of a list

--- src_notebooks/GAN.py
+++ src_notebooks/GAN.py
@@ -703,25 +703,25 @@ def discriminator_block(in_filters, out_filters):
     """Return downsampling layers of each discriminator block"""
     layers = [nn.Conv2d(in_filters, out_filters, 3, stride=2, padding=1)]
     layers.append(nn.LeakyReLU(0.2, inplace=True))
-    return layers
+    return nn.Sequential(*layers)
 
 
 class Discriminator(nn.Module):
     def __init__(self, in_channels=1):
         super(Discriminator, self).__init__()
+        self.layers = nn.Sequential(
+            discriminator_block(in_channels*2, 64),
+            discriminator_block(64, 128),
+            discriminator_block(128, 256),
+            discriminator_block(256, 512),
+            nn.Conv2d(512, 1, 1, stride=4, padding=0),
+        )
 
-        layers = []
-        layers.extend(discriminator_block(in_channels*2, 64))
-        layers.extend(discriminator_block(64, 128))
-        layers.extend(discriminator_block(128, 256))
-        layers.extend(discriminator_block(256, 512))
-        layers.append(nn.Conv2d(512, 1, 4, padding=0))
-        self.model = nn.Sequential(*layers)
 
     def forward(self, img_A, img_B):
         # Concatenate image and condition image by channels to produce input
         img_input = torch.cat((img_A, img_B), 1)
-        return self.model(img_input)
+        return self.layers(img_input)
 
 # %% [markdown]
 # Let's have a look at the architecture of our discriminator:
